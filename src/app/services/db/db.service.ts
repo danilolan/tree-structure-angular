025c484e7b91a4fd1336cd8ebf0c820da51b479f
danilolan <didoherculano@gmail.com>
2023-11-01 17:43:37 -0300

    fix: dbservice methods

--- src/app/services/db/db.service.ts
+++ src/app/services/db/db.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from '@angular/core';
-
+//#TODO create generics
 @Injectable({
   providedIn: 'root'
 })
 export class DbService {
-  setItem(key: string, value: string): void {
-    localStorage.setItem(key, value);
+  setItem(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(value));
   }
 
-  getItem(key: string): string | null {
-    return localStorage.getItem(key);
+  getItem<T>(key: string): any | null {
+    let data = localStorage.getItem(key) || "";
+    return data ? JSON.parse(data) : null;
   }
 
   constructor() { }
